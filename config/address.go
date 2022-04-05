8dae07bd3fc33149b84e82090af7cf12bf088618
mahdi-laptop <imani.mahdi@gmail.com>
2022-04-05 09:45:30 +0430

    feat: adding bitcon addres ...5zp8ss

--- config/address.go
+++ config/address.go
@@ -5,10 +5,10 @@ var AddressList = []map[string]interface{}{
 	// 	"network": ETHEREUM,
 	// 	"address": "0x2bb413fdadbb639584ea33c96a4caa3f5616ca70",
 	// },
-	// {
-	// 	"network": BITCOIN,
-	// 	"address": "bc1qxlatarl0gj2sjfu6pu9g5fdram0jl5c45zp8ss",
-	// },
+	{
+		"network": BITCOIN,
+		"address": "bc1qxlatarl0gj2sjfu6pu9g5fdram0jl5c45zp8ss",
+	},
 	// {
 	// 	"network": BITCOIN,
 	// 	"address": "bc1qjly6e4jj3arp5k67m2sjvc8fdzhtfkk9aazl3u",
